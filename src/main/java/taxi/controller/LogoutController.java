95f16b45665b329ca212382f0d9cea302409a979
Maksym Kulakov <kulakovmaksym0@gmail.com>
2022-10-09 20:28:26 +0200

    ready to hiroku

--- src/main/java/taxi/controller/LogoutController.java
+++ src/main/java/taxi/controller/LogoutController.java
@@ -1,21 +1,22 @@
 package taxi.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import taxi.lib.Injector;
-import taxi.model.Driver;
-import taxi.service.DriverService;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
 
 public class LogoutController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("taxi");
     private static final Logger logger = LogManager.getLogger(LogoutController.class);
-    private final DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+    private final DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
